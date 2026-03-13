6228eaabe9c4b62b10859b1c13129c0ec417b653
tlscksgur <12880924a@gmail.com>
2026-03-13 11:15:48 +0900

    Enhance myPage with book images and rental info
    Updated myPage.php to include book images and rental details.

--- C-Module/views/user/myPage.php
+++ C-Module/views/user/myPage.php
@@ -1,10 +1,23 @@
-<section id="myPage">
-  <?php foreach($myPage as $mp): ?>
-    <div>
-      <p><?= $mp -> book_title ?></p>
-      <p><?= $mp -> book_author ?></p>
-      <p><?= $mp -> book_year ?></p>
-      <p><?= $mp -> book_price ?></p>
-    </div>
-  <?php endforeach; ?>
+<section id="myPage" class="center">
+
+  <div class="myRentBook">
+    <?php foreach($myPage as $mp): ?>
+      <div>
+        <img src="./image/<?= $mp -> book_img ?>">
+        <p><?= $mp -> book_title ?></p>
+        <p><?= $mp -> book_author ?></p>
+        <p><?= $mp -> rentDay ?></p>
+        <p><?= $mp -> returnDay ?></p>
+        <p><?= $mp -> remainDay ?></p>
+      </div>
+    <?php endforeach; ?>
+    <form action="/returnBook" method="post">
+      <input type="submit" value="반납">
+    </form>
+  </div>
+
+  <div class="myRentRoom">
+
+  </div>
+
 </section>
