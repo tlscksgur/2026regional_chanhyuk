8a52f93f1b6bdad8400990d2212b7abfe3e6a511
tlscksgur <12880924a@gmail.com>
2026-03-20 20:52:19 +0900

    지도시작..

--- C-Module/public/js/lib.js
+++ C-Module/public/js/lib.js
@@ -1,2 +1,11 @@
 const $ = (e) => document.querySelector(e)
 const $$ = (e) => [...document.querySelectorAll(e)]
+const $fetch = (path) => fetch(path).then(res => res.json())
+
+const $newEl = (e, a = {}) => Object.assign(document.createElement(e), a)
+
+const newSvg = (e, a = {}) => {
+  const el = document.createElementNS("http://www.w3.org/2000/svg", e);
+  Object.entries(a).forEach(([k, v]) => el.setAttribute(k, v));
+  return el;
+}
---
+++ C-Module/public/js/library.js
@@ -0,0 +1,40 @@
+const $mapContainer = $(".mapContainer");
+
+const libs = await $fetch("./json/도서관현황.json");
+
+await fetch("./southKoreaHigh.svg")
+.then(res => res.text())
+.then(text => {
+  $mapContainer.innerHTML = text
+  return text;
+});
+
+
+const libState = libs.reduce((acc, lib) => {
+  acc[lib["시도명"]] ??= {libCount: 0, seatCount: 0, bookCount: 0};
+  acc[lib["시도명"]].libCount += 1;
+  acc[lib["시도명"]].seatCount += +lib["열람좌석수"];
+  acc[lib["시도명"]].bookCount += +lib["자료수(도서)"];
+  console.log(acc);
+  return acc;
+}, {})
+
+const nameMap = {
+  서울특별시: "서울",
+  부산광역시: "부산",
+  대구광역시: "대구",
+  인천광역시: "인천",
+  광주광역시: "광주",
+  대전광역시: "대전",
+  울산광역시: "울산",
+  경기도: "경기",
+  강원특별자치도: "강원",
+  충청북도: "충북",
+  충청남도: "충남",
+  전라북도: "전북",
+  전라남도: "전남",
+  경상북도: "경북",
+  경상남도: "경남",
+  제주특별자치도: "제주",
+  세종특별자치시: "세종",
+};
